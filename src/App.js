68245b9fe90a97a2506eaad0503c795eb1321008
Karthik4011 <dattu.sai4@gmail.com>
2022-05-23 21:50:41 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Order from './components/order';
+import MapChart from './components/MapChart';
+import MapOrders from './components/MapOrders';
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <ToastContainer />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home/>}>
+          </Route>
+          <Route exact path="/order" element={ <Order/>} /> 
+          <Route exact path="/orderlist" element={ <MapChart/>} /> 
+          <Route exact path='/reslist' element={<MapOrders />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
